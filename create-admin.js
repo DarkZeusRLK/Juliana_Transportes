02b95e08f114cfe85168f96c068fb6b038901168
João Pedro <fernandesjoaopedro622@gmail.com>
2026-05-13 11:34:40 -0300

    att db

---
+++ create-admin.js
@@ -0,0 +1,38 @@
+require('dotenv').config({ path: './.env.local' });
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+const bcrypt = require('bcryptjs');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+async function createAdmin() {
+  try {
+    const senha = 'SUA_SENHA_AQUI';
+
+    const hash = bcrypt.hashSync(senha, 10);
+
+    console.log('HASH GERADO:', hash);
+
+    await pool.query(`
+      INSERT INTO admin (usuario, senha_hash)
+      VALUES ($1, $2)
+      ON CONFLICT (usuario)
+      DO UPDATE SET senha_hash = EXCLUDED.senha_hash
+    `, ['juliana', hash]);
+
+    console.log('Administrador criado com sucesso!');
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+createAdmin();
